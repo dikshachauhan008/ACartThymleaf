55f965d8f2ca858f236d2892faba1e730115ece9
Diksha Chauhan <dikshachauhan008@gmail.com>
2019-06-24 18:46:47 +1000

    Thymeleaf Impliment

---
+++ acart_thymeleaf/src/main/java/com/Ibt/acart/service/UserService.java
@@ -0,0 +1,15 @@
+package com.Ibt.acart.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.Ibt.acart.model.User;
+
+public interface UserService {
+	List<User> userList();
+	User findOne(Long id) throws AcartServiceException;
+	void addUser(User user);
+	String deleteUser(Long id);
+	String updateUser(Long id, User user);
+	String login(User user);
+}
